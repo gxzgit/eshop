0016b1d3c37e0ddf6d2a465bc951b8a30eb72edc
guoxinze <13588031592@qq.com>
2016-09-20 22:48:08 +0800

    aften jiang

--- eeshop/js/onsale_items.js
+++ eeshop/js/onsale_items.js
@@ -1,34 +1,42 @@
 // JavaScript Document
 $(function(){
+	$("#floor_ul>li").click(function(){
+		$(".xd-tag.c").removeClass("c");
+		$(this).addClass("c");
+		$("#add_url").addClass("hide");
+		$("#upload_onsale").addClass("hide");
+		$("#operation").addClass("hide");
+	})
+})
+
+
+
+$(function(){
 	$(".change").click(function(){
-		$("#items_id").val("");
-		$("#result").html("请输入商品编号，验证商品是否在架");
+		$("#url").val("");
+		$("#result").html("请输入点击时打开的页面地址");
 		$("#onsale_picture").attr("src","");
 		if($(this).val()=="更换3"){
-			$("#test").removeClass("hide");
+			$("#add_url").removeClass("hide");
 			$("#upload_onsale").removeClass("hide");
 			$("#operation").removeClass("hide");
 			$("#msg_onsale").html("图片比例约为3:5，宽度不小于300");
+			$("#url").focus()
 		}else{
-			$("#test").removeClass("hide");
+			$("#add_url").removeClass("hide");
 			$("#upload_onsale").removeClass("hide");
 			$("#operation").removeClass("hide");
 			$("#msg_onsale").html("图片比例约为4:5，宽度不小于200");
+			$("#url").focus()
 		}
 	})
 })
 
 
-$(function(){
-	$("#test_items").click(function(){
-		$("#result")
-	})
-})
-
 
 $(function(){
 	$("#cancel").click(function(){
-		$("#test").addClass("hide");
+		$("#add_url").addClass("hide");
 		$("#upload_onsale").addClass("hide");
 		$("#operation").addClass("hide");
 	})
